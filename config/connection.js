e1271de05f9ca9beddf5f3e14eff863a16b4667f
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-08-15 06:54:43 -0500

    updated line I missed

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 
 // Create a Sequelize instance with the database configuration
 const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.JAWSDB_URL) // Use JawsDB URL if available
+  ? new Sequelize(process.env.DATABASE_URL) // Use JawsDB URL if available
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: process.env.DB_HOST, // Set the database host
       dialect: 'postgres', // Set the database dialect to MySQL
