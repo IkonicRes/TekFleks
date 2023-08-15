7a4a2fc608ced88eaf04e5628026a230bc605e15
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-08-15 07:31:56 -0500

    reverted to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -5,16 +5,12 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 // Create a Sequelize instance with the database configuration
-const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL) // Use JawsDB URL if available
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL) // Use JawsDB URL if available
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: process.env.DB_HOST, // Set the database host
-      dialect: 'postgres', // Set the database dialect to MySQL
+      dialect: 'mysql', // Set the database dialect to MySQL
       dialectOptions: {
-        ssl: {
-          require: true, // Require SSL connection
-          rejectUnauthorized: false, // Accept self-signed certificates (for Heroku)
-        },
         decimalNumbers: true, // Enable support for decimal numbers
       },
     });
