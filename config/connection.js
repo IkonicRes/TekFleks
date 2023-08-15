9ad06e5526cfb89738b070ee96f3d4add61b0746
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-08-15 06:12:18 -0500

    changed from JAWS to postgres

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 // Create a Sequelize instance with the database configuration
-const sequelize = process.env.JAWSDB_URL
+const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.JAWSDB_URL) // Use JawsDB URL if available
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: process.env.DB_HOST, // Set the database host
