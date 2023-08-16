52bab07c3338bc4f59fa8e8933201534b213f212
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-08-16 13:35:46 -0500

    more changes

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -3,14 +3,14 @@ const { Comment } = require('../models');
 const commentData = [
   {
     comment_poster_id: 1, // Corresponds to the user ID
-    post_id: 1,
+    post_id: 0,
     likes: 2,
     content: 'This is a comment by user 1.',
   },
   {
     comment_poster_id: 2, // Corresponds to the user ID
     post_id: 1,
-    likes: 3,
+    likes: 0,
     content: 'This is a comment by user 2.',
   },
 ];
