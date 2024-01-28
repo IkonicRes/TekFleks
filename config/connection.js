4dcc09a6eec25ae4d153c1b04329469c4a5c30a5
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2024-01-28 09:23:52 -0600

    Reverted to JawsDB

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 // Create a Sequelize instance with the database configuration
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL) // Use the CR DB URL if available
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL) // Use the CR DB URL if available
+  : new Sequelize(process.env.JAWSDB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: process.env.DB_HOST, // Set the database host
       dialect: 'mysql', // Set the database dialect to MySQL
       dialectOptions: {
